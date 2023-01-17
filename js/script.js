2572733b1ea5aa79c64c918aed17226276f492b9
sandeep <bsandeep650@gmail.com>
2023-01-17 21:05:41 +0530

    add about,social,skills,basic info section in js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 window.addEventListener('load', main);
 //console.log = function() {};
 
-function main() {
-    var xhr = new XMLHttpRequest();
+async function main() {
+    var xhr = await new XMLHttpRequest();
 
     xhr.addEventListener("readystatechange", function() {
         if (this.readyState === 4) {
@@ -20,54 +20,98 @@ function main() {
 }
 // get json data and call to its respective function
 function printJsonData(jsonData) {
-    for (const elem in jsonData) {
-        switch (elem) {
-            case "about":
-                about(jsonData[elem]);
-            case "social":
-                social(jsonData[elem]);
-        }
-        console.log(elem);
-    }
+    about(jsonData['about']);
+    social(jsonData['social']);
+    basicInfo(jsonData['basic_info']);
+    skills(jsonData['skills']);
+    experience(jsonData['experience']);
 }
 
 function about(aboutData) {
     document.querySelector('#profileImg').src = aboutData.profileImage;
     document.querySelector('#name').innerText = aboutData.name;
     document.querySelector('#subLine').innerText = aboutData.developerType;
-    console.log(aboutData);
+    let aboutdatahtml = '';
+    for (const i in aboutData.about) {
+        aboutdatahtml += `<p>${aboutData.about[i]}</p>`;
+
+    }
+    document.getElementById('about-desc').innerHTML = aboutdatahtml;
+
+
 }
 
 function social(socialData) {
-
     let socialIconHtml = '';
     for (const key in socialData) {
         if (Object.hasOwnProperty.call(socialData, key)) {
             const element = socialData[key];
-            socialIconHtml += `<a class="btn btn-default btn-round btn-lg btn-icon" 
-                href="${socialData[key].url}" 
-                rel="tooltip" title="${socialData[key].tooltip}">
-                <i class="${socialData[key].icon}"></i>
-            </a>`;
+            socialIconHtml += `<a class="btn btn-default btn-round btn-lg btn-icon" href="${element.url}" rel="tooltip" title="${element.tooltip}">
+            <i class="${element.icon}"></i>
+        </a>`;
         }
     }
+    document.querySelector('.button-container').innerHTML = '';
     document.querySelector('.button-container').innerHTML = socialIconHtml;
-    console.log(socialIconHtml);
+}
+
+function basicInfo(basicInfodata) {
+    let basicInfoTemplate = '';
+    for (const key in basicInfodata) {
+        if (Object.hasOwnProperty.call(basicInfodata, key)) {
+            // const element = basicInfodata[key];
+            basicInfoTemplate += `<div class="row mt-3">
+                                    <div class="col-sm-4"><strong class="text-uppercase">${key}:</strong></div>
+                                    <div class="col-sm-8">${basicInfodata[key]}</div>
+                                </div>`;
+        }
+    }
+    document.getElementById('basic-info').innerHTML = basicInfoTemplate;
 }
 
 
-(function() {
-    var _z = console;
-    Object.defineProperty(window, "console", {
-        get: function() {
-            if (_z._commandLineAPI) {
-                throw "Sorry, Can't execute scripts!";
-            }
-            return _z;
-        },
-        set: function(val) {
-            _z = val;
-        }
-    });
+//adding skills info
 
-})();
+function skills(skillsData) {
+    let skillshtml = `<div class="row">`;;
+    let i = 0;
+    for (const key in skillsData) {
+        if (Object.hasOwnProperty.call(skillsData, key)) {
+
+            if (i % 2 != 0) {
+                skillshtml += `<div class="col-md-6">
+                <div class="progress-container progress-primary"><span class="progress-badge">${key}</span>
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-primary aos-init aos-animate" data-aos="progress-full" data-aos-offset="10" data-aos-duration="2000" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: ${skillsData[key]};"></div><span class="progress-value">${skillsData[key]}</span>
+                    </div>
+                </div>
+            </div>`;
+            } else {
+                skillshtml += `<div class="col-md-6">
+                <div class="progress-container progress-primary"><span class="progress-badge">${key}</span>
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-primary aos-init aos-animate" data-aos="progress-full" data-aos-offset="10" data-aos-duration="2000" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: ${skillsData[key]};"></div><span class="progress-value">${skillsData[key]}</span>
+                    </div>
+                </div>
+            </div>`;
+            }
+
+
+        }
+
+        i++;
+    }
+    skillshtml += `</div>`;
+    document.getElementById('skills-info').innerHTML = skillshtml;
+}
+
+function experience(experienceData) {
+    for (const key in experienceData) {
+        if (Object.hasOwnProperty.call(experienceData, key)) {
+            const element = experienceData[key];
+            console.log(element);
+
+        }
+    }
+
+}
